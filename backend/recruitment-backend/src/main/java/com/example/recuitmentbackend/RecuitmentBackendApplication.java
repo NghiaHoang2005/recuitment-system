2647b9db57652a83f5e61f5cf75e9099a0a4c431
chun2005 <hoangquangnghia123@gmail.com>
2026-02-09 17:21:43 +0700

    initialize Spring Boot backend

---
+++ backend/recruitment-backend/src/main/java/com/example/recuitmentbackend/RecuitmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.recuitmentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecuitmentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecuitmentBackendApplication.class, args);
+    }
+
+}
---
+++ backend/recruitment-backend/src/test/java/com/example/recuitmentbackend/RecuitmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recuitmentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecuitmentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
